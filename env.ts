a2fbd28d39dd773e4f0d8bd16229e1be0c13f7de
esize <evan@wool.homes>
2025-02-25 22:41:37 -0500

    add t3-env

---
+++ env.ts
@@ -0,0 +1,31 @@
+/* eslint-disable n/no-process-env */
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.string().url(),
+    NODE_ENV: z.enum(["development", "test", "production"]),
+  },
+  client: {
+    NEXT_PUBLIC_APP_NAME: z.string().min(1).default("Template"),
+  },
+  runtimeEnv: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    NODE_ENV: process.env.NODE_ENV,
+    NEXT_PUBLIC_APP_NAME: process.env.NEXT_PUBLIC_APP_NAME,
+  },
+  // Called when the schema validation fails.
+  onValidationError: (error) => {
+    console.error("❌ Invalid environment variables:", error);
+    throw new Error("Invalid environment variables");
+  },
+  // Called when server variables are accessed on the client.
+  onInvalidAccess: () => {
+    throw new Error(
+      "❌ Attempted to access a server-side environment variable on the client",
+    );
+  },
+
+  emptyStringAsUndefined: true,
+});
